17e37035065ffa759e55055ff215eea6f58b3fdf
zzyu <zhengzhiyu825@163.com>
2020-01-04 11:25:22 +0800

    new change

--- index.js
+++ index.js
@@ -7,9 +7,13 @@
 // git init 本地仓库初始化 （rm -rf .git 强制删库）文件夹中没有的话点查看-隐藏项目
 // git add index.html 将文件放入暂存区 暂存区作用（1、为文件创建索引 2、进行分类）
 // git commit -m 'project init' 作为一个版本提交到本地仓库
-// -m参数是输入提交信息的 -am参数就是可以把还没有执行add命令的修改一起提交
+// -m参数是输入提交信息的 -am参数就是可以把还没有执行add命令的修改一起提交 --amend修改上一次提交信息
 // git add . 所有文件全部放入暂存区
 // git status 查看状态
 // git checkout -- index.html 撤销(文件中修改的内容)操作
 // git log 查看日志记录
 // git log --graph 图形化展示
+// git branch zzy 创建自己的分支
+// git branch 查看分支
+// git checkout zzy 切换分支
+// git checkout -b zzy1 创建并切换分支
